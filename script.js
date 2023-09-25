b0508141ac99adffc11191a687c90df38d72a6dd
Stanley Azi <azistanley17@gmail.com>
2023-09-25 18:58:43 +0100

    new commit

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 const features = document.querySelector(".nav-features");
 const company = document.querySelector(".nav-company");
 
-const featuresArrowUp = document.querySelector(".arrow-up-1");
-const featuresArrowDown = document.querySelector(".arrow-down-1");
-const companyArrowUp = document.querySelector(".arrow-up-2");
-const companyArrowDown = document.querySelector(".arrow-down-2");
+const featuresArrow = document.getElementById("features-arrow");
+const companyArrow = document.getElementById("company-arrow");
 
-const featuresList = document.querySelector("features-list");
-const companyList = document.querySelector("company-list");
+const featuresList = document.getElementById("features-list");
+const companyList = document.getElementById("company-list");
+
+//toggling features dropdown
+const toggleFeatures = () => {
+    features.addEventListener('click', function() {
+        featuresArrow.classList.toggle("arrow-down-1");
+        featuresList.classList.toggle("show-features");
+    });
+};
+toggleFeatures();
+//
+
+//toggling company dropdown
+const toggleCompany = () => {
+    company.addEventListener('click', function(e) {
+        e.stopPropagation();
+        companyArrow.classList.toggle("arrow-down-2");
+        companyList.classList.toggle("show-company-ul");
+    });
+};
+toggleCompany();
+//
+
+// document.documentElement.addEventListener('click', function() {
+//     if(companyList.classList.contains("show-company-ul")) {
+//         toggleCompany();
+//     };
+// });
