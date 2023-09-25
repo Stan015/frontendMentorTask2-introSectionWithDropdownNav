0c1e45921f518c4e5318596a7514c05512306932
Stanley Azi <azistanley17@gmail.com>
2023-09-25 12:41:47 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const features = document.querySelector(".nav-features");
+const company = document.querySelector(".nav-company");
+
+const featuresArrowUp = document.querySelector(".arrow-up-1");
+const featuresArrowDown = document.querySelector(".arrow-down-1");
+const companyArrowUp = document.querySelector(".arrow-up-2");
+const companyArrowDown = document.querySelector(".arrow-down-2");
+
+const featuresList = document.querySelector("features-list");
+const companyList = document.querySelector("company-list");
